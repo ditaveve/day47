1cb922df1a6132e5caa716e447eb58849a303543
Diana Angheluta <dianaangheluta@Dianas-MacBook-Pro.local>
2026-07-08 04:01:13 +0300

    small update: some readability improvements

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from scrape_price import ScrapePrice
+from notification_manager import NotificationManager
 
-scraper = ScrapePrice()
+TARGET_URL = "https://appbrewery.github.io/instant_pot/"
+TARGET_PRICE = 100
+
+scraper = ScrapePrice(TARGET_URL)
 price = scraper.scrape_price()
+product_full_name = scraper.scrape_product_full_name()
 
+if price < TARGET_PRICE:
+    notification_manager = NotificationManager()
+    message = f"Subject:AMAZON PRICE ALERT!\n\n{product_full_name} is now ${price}\n{TARGET_URL}"
+    notification_manager.send_emails(message=message)
 print(price)

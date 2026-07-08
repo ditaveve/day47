a418ab73715d1b4248e5540eebd62c69bed59e21
Diana Angheluta <dianaangheluta@Dianas-MacBook-Pro.local>
2026-07-08 04:26:39 +0300

    small update: switched from practice static url to a live url

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from scrape_price import ScrapePrice
 from notification_manager import NotificationManager
 
-TARGET_URL = "https://appbrewery.github.io/instant_pot/"
-TARGET_PRICE = 100
+TARGET_URL = "https://www.amazon.com/dp/B0DNF49K1D/ref=sspa_dk_detail_2?pd_rd_i=B0DNF49K1D&pd_rd_w=orORV&content-id=amzn1.sym.3bc66c0a-cc61-4816-aa2d-e53327eaddb6&pf_rd_p=3bc66c0a-cc61-4816-aa2d-e53327eaddb6&pf_rd_r=FJHYQSRACMHXYCS90Y9R&pd_rd_wg=yKNSS&pd_rd_r=4a259fbb-a72d-497c-a133-864aac82d0c3&sp_csd=d2lkZ2V0TmFtZT1zcF9kZXRhaWxfdGhlbWF0aWM&th=1"
+TARGET_PRICE = 1000
+TARGET_PRICE_CURRENCY = "RON"
 
 scraper = ScrapePrice(TARGET_URL)
 price = scraper.scrape_price()
@@ -10,6 +11,6 @@ product_full_name = scraper.scrape_product_full_name()
 
 if price < TARGET_PRICE:
     notification_manager = NotificationManager()
-    message = f"Subject:AMAZON PRICE ALERT!\n\n{product_full_name} is now ${price}\n{TARGET_URL}"
+    message = f"Subject:AMAZON PRICE ALERT!\n\n{product_full_name} is now {price}{TARGET_PRICE_CURRENCY}\n{TARGET_URL}"
     notification_manager.send_emails(message=message)
 print(price)
